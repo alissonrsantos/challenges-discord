a48e7a829ab2509ae519799ec1ca9617dae7a813
romaoaveiro <alisson_romao@outlook.com>
2021-07-05 13:55:52 -0300

    Challengers

---
+++ blog/src/js/script.js
@@ -0,0 +1,11 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('.post', { delay: 200 });
+ScrollReveal().reveal('.details-post', { delay: 400 });
+ScrollReveal().reveal('.post-title', { delay: 500 });
+ScrollReveal().reveal('.post-description', { delay: 600 });
